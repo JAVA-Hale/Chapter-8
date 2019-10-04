ba9f451fa74898c6190ab633d20182d2f24da7fc
Thomas hale <TH146688@davenportschools.org>
2019-10-04 09:35:20 -0500

    update change

--- src/YouDoIt/ArrayMethodDemo.java
+++ src/YouDoIt/ArrayMethodDemo.java
@@ -9,25 +9,55 @@ public class ArrayMethodDemo {
         displayHighLow(integerArray);
         displayLowHigh(integerArray);
         displaySum(integerArray);
+        displayLessThan(integerArray);
+        displayAverage(integerArray);
 
     }
+
     public static void displayHighLow(int[] integerArray) {
         for (int i = 0; i < 10; i++) {
             System.out.println(integerArray[i]);
         }
 
-        }
-    public static void displayLowHigh( int[] integerArray){
+    }
+
+    public static void displayLowHigh(int[] integerArray) {
         for (int i = 9; i >= 0; i--) {
             System.out.println(integerArray[i]);
         }
     }
-    public static void displaySum( int[] integerArray){
+
+    public static void displaySum(int[] integerArray) {
         int total = 0;
-        for(int j=0; j<integerArray.length; ++j){
+        for (int j = 0; j < integerArray.length; j++) {
             total = total + integerArray[j];
-                System.out.println("Total = " + integerArray[j]);
+
+        }
+        System.out.println("Total = " + total);
+    }
+
+
+    public static void displayLessThan(int[] integerArray) {
+        for (int j = 0; j < integerArray.length; j++) {
+            if (integerArray[j] < 10) {
+                System.out.println(integerArray[j]);
             }
         }
 
+
     }
+    public static void displayAverage(int[] integerArray)
+    {
+        int total = 55;
+        int average = total/ integerArray.length;
+        for (int j = 0; j < integerArray.length; j++) {
+            if(integerArray[j] > average){
+            }
+        }
+        System.out.println(average);
+
+    }
+    }
+
+
+
