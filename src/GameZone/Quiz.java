8384c4456f91f732a44861b22e9eaaf972058616
Thomas hale <TH146688@davenportschools.org>
2019-10-08 08:56:32 -0500

    update change

--- src/GameZone/Quiz.java
+++ src/GameZone/Quiz.java
@@ -10,23 +10,24 @@ public class Quiz {
         int score = 0;
 
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("What is 2 + 2? \n A- for 22 " +
-                "\n B- for 8 \n C - for 4 \n choice >>>>");
-        choice = input.next();
-        if (choice.equalsIgnoreCase("A")) {
-            System.out.println("The correct answer is C");
-        } else if (choice.equalsIgnoreCase("B")) {
-            System.out.println("The correct answer is C ");
-            score++;
-        } else if (choice.equalsIgnoreCase("C")) {
-            System.out.println("Correct");
+        while (score < 3) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("What is 2 + 2? \n A- for 22 " +
+                    "\n B- for 8 \n C - for 4 \n choice >>>>");
+            choice = input.next();
+            if (choice.equalsIgnoreCase("A")) {
+                System.out.println("The correct answer is C");
+            } else if (choice.equalsIgnoreCase("B")) {
+                System.out.println("The correct answer is C ");
+            } else if (choice.equalsIgnoreCase("C")) {
+                System.out.println("Correct");
+                score++;
 
-        }
+            }
 
-        System.out.println("What is 4 - 2? \n A- for 42 " +
+            System.out.println("What is 4 - 2? \n A- for 42 " +
                     "\n B- for 2 \n C - for 6 \n choice >>>>");
-        choice = input.next();
+            choice = input.next();
             if (choice.equalsIgnoreCase("A")) {
                 System.out.println("The correct answer is B");
             } else if (choice.equalsIgnoreCase("B")) {
@@ -35,19 +36,104 @@ public class Quiz {
             } else if (choice.equalsIgnoreCase("C")) {
                 System.out.println("The correct answer is B");
             }
-        System.out.println("What is 2 X 3? \n A- for 5 " +
-                "\n B- for 2 \n C - for 6 \n choice >>>>");
-        choice = input.next();
-        if (choice.equalsIgnoreCase("A")) {
-            System.out.println("The correct answer is C");
-        } else if (choice.equalsIgnoreCase("B")) {
-            System.out.println("The correct answer is C ");
-            score++;
-        } else if (choice.equalsIgnoreCase("C")) {
-            System.out.println("Correct");
-        }
 
+            System.out.println("What is 2 X 3? \n A- for 5 " +
+                    "\n B- for 2 \n C - for 6 \n choice >>>>");
+            choice = input.next();
+            if (choice.equalsIgnoreCase("A")) {
+                System.out.println("The correct answer is C");
+            } else if (choice.equalsIgnoreCase("B")) {
+                System.out.println("The correct answer is C ");
+            } else if (choice.equalsIgnoreCase("C")) {
+                System.out.println("Correct");
+                score++;
+            }
 
+            System.out.println("What is 5 X 5? \n A- for 25 " +
+                    "\n B- for 10 \n C - for 1 \n choice >>>>");
+            choice = input.next();
+            if (choice.equalsIgnoreCase("A")) {
+                System.out.println("Correct");
+                score++;
+            } else if (choice.equalsIgnoreCase("B")) {
+                System.out.println("The correct answer is A");
+            } else if (choice.equalsIgnoreCase("C")) {
+                System.out.println("The correct answer is A");
+            }
+
+            System.out.println("What is 5 + 5? \n A- for 25 " +
+                    "\n B- for 10 \n C - for 1 \n choice >>>>");
+            choice = input.next();
+            if (choice.equalsIgnoreCase("A")) {
+                System.out.println("The correct answer is B");
+            } else if (choice.equalsIgnoreCase("B")) {
+                System.out.println("Correct");
+                score++;
+            } else if (choice.equalsIgnoreCase("C")) {
+                System.out.println("The correct answer is B");
+            }
+
+            System.out.println("What is 5 - 3? \n A- for 2 " +
+                    "\n B- for 10 \n C - for 8 \n choice >>>>");
+            choice = input.next();
+            if (choice.equalsIgnoreCase("A")) {
+                System.out.println("Correct");
+                score++;
+            } else if (choice.equalsIgnoreCase("B")) {
+                System.out.println("The correct answer is A");
+            } else if (choice.equalsIgnoreCase("C")) {
+                System.out.println("The correct answer is A");
+            }
+
+            System.out.println("What is 5 X 15? \n A- for 20 " +
+                    "\n B- for 75 \n C - for 10 \n choice >>>>");
+            choice = input.next();
+            if (choice.equalsIgnoreCase("A")) {
+                System.out.println("The correct answer is B");
+            } else if (choice.equalsIgnoreCase("B")) {
+                System.out.println("Correct");
+                score++;
+            } else if (choice.equalsIgnoreCase("C")) {
+                System.out.println("The correct answer is B");
+            }
+
+            System.out.println("What is 20 X 30? \n A- for 6000 " +
+                    "\n B- for 60 \n C - for 600 \n choice >>>>");
+            choice = input.next();
+            if (choice.equalsIgnoreCase("A")) {
+                System.out.println("The correct answer is C");
+            } else if (choice.equalsIgnoreCase("B")) {
+                System.out.println("The correct answer is C ");
+            } else if (choice.equalsIgnoreCase("C")) {
+                System.out.println("Correct");
+                score++;
+            }
+
+            System.out.println("What is 4000 / 2000? \n A- for 20 " +
+                    "\n B- for 200 \n C - for 2 \n choice >>>>");
+            choice = input.next();
+            if (choice.equalsIgnoreCase("A")) {
+                System.out.println("The correct answer is C");
+            } else if (choice.equalsIgnoreCase("B")) {
+                System.out.println("The correct answer is C ");
+            } else if (choice.equalsIgnoreCase("C")) {
+                System.out.println("Correct");
+                score++;
+            }
+
+            System.out.println("What is (2 X 3 X 4 X 0) + 1? \n A- for 24 " +
+                    "\n B- for 1 \n C - for 6 \n choice >>>>");
+            choice = input.next();
+            if (choice.equalsIgnoreCase("A")) {
+                System.out.println("The correct answer is B");
+            } else if (choice.equalsIgnoreCase("B")) {
+                System.out.println("Correct");
+                score++;
+            } else if (choice.equalsIgnoreCase("C")) {
+                System.out.println("The correct answer is B");
+            }
+            System.out.println("Your score is " + score + " out of 10");
 
             }
         }
+    }
